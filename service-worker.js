d2151f964db9360a948b009cf48dfa5cec2a7cf9
Jack Rademacher <jackradema@gmail.com>
2020-07-16 16:49:59 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.20f98ebad4e2a77015359c526d93f345.js"
+  "/precache-manifest.6bced148f0569ba945552189cb572dee.js"
 );
 
 self.addEventListener('message', (event) => {
